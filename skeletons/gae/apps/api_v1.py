c5d9d51201bcd8634e8a631a8d31891e21b9bb73
Yaogang Lian <yaogang.lian@gmail.com>
2013-09-17 10:51:18 -0400

    Updated gae boilerplate.

--- skeletons/gae/apps/api_v1.py
+++ skeletons/gae/apps/api_v1.py
@@ -5,17 +5,16 @@ import webapp2 as webapp
 from apps.decorators import *
 from apps.models import *
 from apps import DEBUG
-import json, re, logging, datetime
+import json, re, logging
 
-# GET /api
+# GET /api/v1/
 class MainHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write('OK')
 
-#
 # Router
-#
+baseUrl = '/api/v1'
 routes = [
-    (r'/api', MainHandler),
+    (r'^%s/$' % baseUrl, MainHandler),
 ]
 app = webapp.WSGIApplication(routes, debug=DEBUG)
